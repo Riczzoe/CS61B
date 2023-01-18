cef4499c00cc1a6dd08c2e70073e92415c42441c
FrankJohn <86246881+JohnFrankz@users.noreply.github.com>
2023-01-18 19:07:55 +0800

    finish lab2

---
+++ Lection/List/src/SLList/SLList.java
@@ -0,0 +1,66 @@
+package SLList;
+
+public class SLList {
+
+    public static class IntNode {
+        public int item;
+        public IntNode next;
+
+        public IntNode(int item, IntNode next) {
+            this.item = item;
+            this.next = next;
+        }
+    }
+
+    private IntNode sentinel;
+    private int size;
+
+    public SLList() {
+        sentinel = new IntNode(-63, null);
+        size = 0;
+    }
+
+    public SLList(int x) {
+        sentinel = new IntNode(-63, null);
+        sentinel.next = new IntNode(x, null);
+        size = 1;
+    }
+
+    public void addFirst(int x) {
+        sentinel.next = new IntNode(x, sentinel.next);
+        size++;
+    }
+
+    public int getFirst() {
+        return sentinel.next.item;
+    }
+
+    public static  int size(IntNode p) {
+        if (p.next == null) {
+            return 1;
+        }
+        return 1 + size(p.next);
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void addLast(int x) {
+        IntNode p = sentinel;
+
+        while (p.next != null) {
+            p = p.next;
+        }
+
+        p.next = new IntNode(x, null);
+        size++;
+    }
+
+    public static void main(String[] args) {
+        SLList s = new SLList(3);
+        s.addFirst(2);
+        s.addFirst(1);
+        s.addLast(4);
+    }
+}
--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,30 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+
+    @Test
+    public void testSquarePrimesNoPrime() {
+        IntList lst = IntList.of(14, 15, 16, 12, 18);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("14 -> 15 -> 16 -> 12 -> 18", lst.toString());
+        assertFalse(changed);
+    }
+
+
+    @Test
+    public void testSquarePrimesTwoPrimes() {
+        IntList lst = IntList.of(7, 15, 16, 17, 18);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("49 -> 15 -> 16 -> 289 -> 18", lst.toString());
+        assertTrue(changed);
+    }
+
+    @Test
+    public void testSquarePrimesAllPrimes() {
+        IntList lst = IntList.of(2, 3, 5, 7, 11);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("4 -> 9 -> 25 -> 49 -> 121", lst.toString());
+        assertTrue(changed);
+    }
+
 }
